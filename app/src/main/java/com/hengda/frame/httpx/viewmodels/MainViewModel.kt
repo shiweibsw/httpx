6f61ff8640657b39c1555c0cfcf1732ac1f391d0
shiweibsw <shiweibsw@gmail.com>
2021-06-28 11:57:52 +0800

    0628  增加loading状态监听

--- app/src/main/java/com/hengda/frame/httpx/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/hengda/frame/httpx/viewmodels/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.hengda.frame.httpx.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
