eb7828690953594f4da4249d7161aed81fb37290
shiweibsw <shiweibsw@gmail.com>
2021-07-05 13:04:57 +0800

    github

---
+++ library/src/main/java/com/github/frame/httpx/library/config/Config.kt
@@ -0,0 +1,11 @@
+package com.github.frame.httpx.library.config
+
+/**
+ * @Author shiwei
+ * @Date 2021/6/23-9:25
+ * @Email shiweibsw@gmail.com
+ */
+const val DEFAULT_TIMEOUT: Long = 10L
+const val DEFAULT_SUCCESS_CODE = 1
+const val DEFAULT_LOGGER_TAG = "HttpManager"
+const val DOMAIN = "Domain-Name"
---
+++ library/src/main/java/com/github/frame/httpx/library/response/ApiResponse.kt
@@ -0,0 +1,40 @@
+package com.github.frame.httpx.library.response
+
+class ApiResponse<T> {
+    private var code: Int = 0
+    private var msg: String? = null
+    private var data: T? = null
+
+    fun getCode(): Int {
+        return code
+    }
+
+    fun setCode(code: Int) {
+        this.code = code
+    }
+
+    fun getMsg(): String {
+        return msg ?: ""
+    }
+
+    fun setMsg(msg: String) {
+        this.msg = msg
+    }
+
+    fun getDatas(): T? {
+        return data
+    }
+
+    fun setDatas(datas: T) {
+        this.data = datas
+    }
+
+    override fun toString(): String {
+        val sb = StringBuffer()
+        sb.append("code=$code msg=$msg")
+        if (null != data) {
+            sb.append(" data:" + data.toString())
+        }
+        return sb.toString()
+    }
+}
